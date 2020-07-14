e686502672d56e344638e100af8c13ce7aa18e1a
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-07-14 14:40:30 +0300

    Updates

---
+++ precache-manifest.caad154b347a78460d574a9ad567a86e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0b2f971acb5f1264a6fba817e32901",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "cbf58d887d0145f6ef8b",
+    "url": "/Portfolio/static/css/2.7e009a7e.chunk.css"
+  },
+  {
+    "revision": "4a38200f835bb4abc92b",
+    "url": "/Portfolio/static/css/main.2d407269.chunk.css"
+  },
+  {
+    "revision": "cbf58d887d0145f6ef8b",
+    "url": "/Portfolio/static/js/2.60e238f7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Portfolio/static/js/2.60e238f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a38200f835bb4abc92b",
+    "url": "/Portfolio/static/js/main.75841c59.chunk.js"
+  },
+  {
+    "revision": "bbf9f513ca94ccde67a9",
+    "url": "/Portfolio/static/js/runtime-main.88d4256f.js"
+  },
+  {
+    "revision": "268a06c3adc949875ea1b1c0b02133d0",
+    "url": "/Portfolio/static/media/BahaeddinDabbaghCV.268a06c3.pdf"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Portfolio/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Portfolio/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "1191582522e82c53b70b4348c9316325",
+    "url": "/Portfolio/static/media/contract.11915825.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/Portfolio/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/Portfolio/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/Portfolio/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/Portfolio/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/Portfolio/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/Portfolio/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/Portfolio/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/Portfolio/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/Portfolio/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/Portfolio/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/Portfolio/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/Portfolio/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/Portfolio/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/Portfolio/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/Portfolio/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "6db0e97131dfe6c3c611dde5b0b7e93c",
+    "url": "/Portfolio/static/media/learnGo.6db0e971.png"
+  },
+  {
+    "revision": "329000df1c2d754b1948ca487ac4aca5",
+    "url": "/Portfolio/static/media/madlib.329000df.jpg"
+  },
+  {
+    "revision": "6713149658e263f34313dfe540527e92",
+    "url": "/Portfolio/static/media/movieProject.67131496.jpg"
+  },
+  {
+    "revision": "ff4d2049f17ca42e6f9a5416a632445d",
+    "url": "/Portfolio/static/media/personal site.ff4d2049.svg"
+  },
+  {
+    "revision": "462cc15224031a8f3b5fbc27960cd349",
+    "url": "/Portfolio/static/media/portfolio.462cc152.jpg"
+  },
+  {
+    "revision": "1991d3fe2d9a8f1d108f98cde5590b4a",
+    "url": "/Portfolio/static/media/project.1991d3fe.svg"
+  },
+  {
+    "revision": "5046fb65b3b0a66e13821ecc509f27bd",
+    "url": "/Portfolio/static/media/underConstruction.5046fb65.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.764aafe5049d884bd66d7937a22aa016.js"
+  "/Portfolio/precache-manifest.caad154b347a78460d574a9ad567a86e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
