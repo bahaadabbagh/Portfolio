771800774e016c596d42dd3817ce5c9c4cde2560
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-08-25 13:28:22 +0300

    added WWW

--- src/components/oneCard.js
+++ src/components/oneCard.js
@@ -1,16 +1,59 @@
 import React from "react";
-import {MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBRow, MDBIcon, MDBContainer} from "mdbreact";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBContainer,
+} from "mdbreact";
 import movieProject from "../images/movieProject.jpg";
 import madlib from "../images/madlib.jpg";
-import atoproject from "../images/ATO.jpg"
+import atoproject from "../images/ATO.jpg";
 import learngo from "../images/learnGo.png";
 import portfolioScreenshot from "../images/portfolio.jpg";
+import WWW from "../images/WWW.jpg";
 
 export default function OneCard(props) {
   return (
     <MDBContainer>
       <MDBRow>
-      <MDBCol  sm="6">
+        <MDBCol sm="6">
+          <MDBCard className="oneCard">
+            <MDBCardImage className="img-fluid" src={WWW} waves />
+            <MDBCardBody>
+              <MDBCardTitle>World With Women</MDBCardTitle>
+              <MDBCardText>
+                A platform to inspire women around the world and share their
+                struggles and achievements
+                <br />
+                <br />
+                <strong>HTML, CSS, React, Bootstrap,WordPress API</strong>
+              </MDBCardText>
+              <div className="cardButtons">
+                <a
+                  href="https://github.com/ReCoded-Org/capstone-istanbul-dijitalWorld"
+                  target="_blank"
+                  className="px-2 fa-lg fb-ic"
+                >
+                  <MDBIcon fab icon="github-square" size="3x" />
+                </a>
+                <MDBBtn
+                  color="purple"
+                  href="https://dijitalworld.netlify.app/"
+                  target="_blank"
+                >
+                  Visit Website
+                </MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol sm="6">
           <MDBCard className="oneCard">
             <MDBCardImage className="img-fluid" src={atoproject} waves />
             <MDBCardBody>
@@ -18,7 +61,7 @@ export default function OneCard(props) {
               <MDBCardText>
                 Made this page for a job application in 2 days
                 <br />
-                <br/>
+                <br />
                 <strong>HTML, CSS, React, Bootstrap, API</strong>
               </MDBCardText>
               <div className="cardButtons">
@@ -40,15 +83,16 @@ export default function OneCard(props) {
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
-        <MDBCol  sm="6">
+        <MDBCol sm="6">
           <MDBCard className="oneCard">
             <MDBCardImage className="img-fluid" src={madlib} waves />
             <MDBCardBody>
               <MDBCardTitle>Madlibs</MDBCardTitle>
               <MDBCardText>
-              Madlibs has never been MADDER with this rendition of the classic mad libs .
-                <br/>
-                <br/>
+                Madlibs has never been MADDER with this rendition of the classic
+                mad libs .
+                <br />
+                <br />
                 <strong>HTML, CSS, JavaScript</strong>
               </MDBCardText>
               <div className="cardButtons">
@@ -76,9 +120,9 @@ export default function OneCard(props) {
             <MDBCardBody>
               <MDBCardTitle>LearnGo</MDBCardTitle>
               <MDBCardText>
-              A task organizing app to help students in the Re:Coded bootcamp.
-              <br />
-              <br/>
+                A task organizing app to help students in the Re:Coded bootcamp.
+                <br />
+                <br />
                 <strong>CSS, JSX, React, Bootstrap, FireBase</strong>
               </MDBCardText>
               <div className="cardButtons">
@@ -100,7 +144,7 @@ export default function OneCard(props) {
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
-        <MDBCol  sm="6">
+        <MDBCol sm="6">
           <MDBCard className="oneCard">
             <MDBCardImage
               className="img-fluid"
@@ -110,9 +154,9 @@ export default function OneCard(props) {
             <MDBCardBody>
               <MDBCardTitle>My Portfolio</MDBCardTitle>
               <MDBCardText>
-                The website you're already on! intoriducing you to who I am 
+                The website you're already on! intoriducing you to who I am
                 <br />
-                <br/>
+                <br />
                 <strong>JSX, React, Bootstrap</strong>
               </MDBCardText>
               <div className="cardButtons">
@@ -130,17 +174,16 @@ export default function OneCard(props) {
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
-      </MDBRow>
-      <MDBRow>
-      <MDBCol  sm="6">
+        <MDBCol sm="6">
           <MDBCard className="oneCard">
             <MDBCardImage className="img-fluid" src={movieProject} waves />
             <MDBCardBody>
               <MDBCardTitle>CinemaBox</MDBCardTitle>
               <MDBCardText>
-                This project uses the TMDB API to search for movie and actor details.
+                This project uses the TMDB API to search for movie and actor
+                details.
                 <br />
-                <br/>
+                <br />
                 <strong>HTML, CSS, JavaScript, Bootstrap, API</strong>
               </MDBCardText>
               <div className="cardButtons">
@@ -162,7 +205,7 @@ export default function OneCard(props) {
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
-        </MDBRow>
+      </MDBRow>
     </MDBContainer>
   );
 }
